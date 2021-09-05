e007b905772dcc137d8ff99106443c5a025ca276
Cleonart <zredhard@gmail.com>
2021-09-05 15:23:41 +0800

    added model and controller

---
+++ models/material.py
@@ -0,0 +1,10 @@
+class Material:
+	"""Class for material model"""
+	def __init__(self, obj):
+		""" Initialisation of material model """
+		self.id = ""
+		self.name = ""
+		self.buy_price = 0
+		self.type_id = ""
+		self.supplier_id = ""	
+		
